38076f3518907f156fa3d7f7e7001c7878ad43f6
EvyHaan <evy.haan@gmail.com>
2019-01-02 21:23:20 -0800

    function to add book to a collection running. error in retrieving description from database

--- books.sql
+++ books.sql
@@ -27,3 +27,11 @@ INSERT INTO books (author, title, isbn, image_url, description, bookshelf) VALUE
   'Turning the envelope over, his hand trembling, Harry saw a purple wax seal bearing a coat of arms; a lion, an eagle, a badger and a snake surrounding a large letter ''H''. Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle. Then, on Harry''s eleventh birthday, a great beetle-eyed giant of a man called Rubeus Hagrid bursts in with some astonishing news: Harry Potter is a wizard, and he has a place at Hogwarts School of Witchcraft and Wizardry. An incredible adventure is about to begin! Pottermore has now launched the Wizarding World Book Club. Visit Pottermore to sign up and join weekly Twitter discussions at WW Book Club.',
   'Fantasy'
 );
+
+INSERT INTO books (author, title, isbn, image_url, description, bookshelf) VALUES (
+  'Norman Bridwell',
+  'Clifford and the Big Parade',
+  'ISBN_139780545223232',
+  'http://books.google.com/books/content?id=_INUJz3vPZYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  'Clifford\'s town celebrates its centennial, with lots of help from the big red dog.'
+);
--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.set('view engine', 'ejs');
 app.get('/', home);
 app.get('/new', newSearch);
 app.post('/searches', search);
-
 app.get('/books/:id', visitBookDetail);
+app.post('/selectbook', saveBook);
 
 
 //database
@@ -32,7 +32,6 @@ function home(request, response) {
         response.render('pages/searches/new')
       } else {
         response.render('pages/index', {books:result.rows});//get it from the database
-        console.log({books:result.rows});
       }
     })
     .catch( err => {
@@ -56,7 +55,6 @@ function search(request, response) {
     .then( result => {
       let books = result.body.items.map(book => new Book(book));
       response.render('pages/searches/show', {books});
-      console.log(books);
     })
     .catch( err => {
       console.log('superagent error')
@@ -68,14 +66,6 @@ function newSearch (request, response){
   response.render('pages/searches/new');
 }
 
-function Book(book) {
-  console.log(book);
-  this.author = book.volumeInfo.authors || 'Author Unknown';
-  this.title = book.volumeInfo.title || 'Title Missing';
-  this.isbn = book.volumeInfo.industryIdentifiers[0].type + book.volumeInfo.industryIdentifiers[0].identifier || 'ISBN Missing';
-  this.image_url = book.volumeInfo.imageLinks.thumbnail || 'https://i.imgur.com/J5LVHEL.jpeg';
-  this.description = book.volumeInfo.description || 'Description Missing';
-}
 
 function handleError (err, response) {
   console.error(err);
@@ -84,11 +74,9 @@ function handleError (err, response) {
 
 function visitBookDetail(request, response) {
   let SQL = 'SELECT * FROM books where id=$1';
-  console.log('this is the value', [request.params.id]);
   let values = [request.params.id];
   return client.query(SQL, values)
     .then(result => {
-      console.log(result.rows[0])
       response.render('pages/books/show', {selected_book:result.rows[0]}
       )
     })
@@ -98,5 +86,40 @@ function visitBookDetail(request, response) {
     })
 }
 
+
+function saveBook(request, response) {
+  console.log(request.body);
+  let newBook = new BookshelfBook(request.body);
+  let bookArray = Object.values(newBook);
+  bookArray.pop();
+  let SQL = `INSERT INTO books(author, title, isbn, image_url, description, bookshelf)
+  VALUES($1, $2, $3, $4, $5, $6)`
+  console.log('the book array is:', bookArray)
+  return client.query(SQL, bookArray)
+    .then( () => response.redirect('/'))
+    .catch( err => {
+      console.log('database input error')
+      return handleError(err, response);
+    })
+}
+
+function Book(book) {
+  this.author = book.volumeInfo.authors || 'Author Unknown';
+  this.title = book.volumeInfo.title || 'Title Missing';
+  this.isbn = book.volumeInfo.industryIdentifiers[0].type + book.volumeInfo.industryIdentifiers[0].identifier || 'ISBN Missing';
+  this.image_url = book.volumeInfo.imageLinks.thumbnail || 'https://i.imgur.com/J5LVHEL.jpeg';
+  this.description = book.volumeInfo.description || 'Description Missing';
+}
+
+function BookshelfBook(book, bookshelf) {
+  this.author = book.author;
+  this.title = book.title;
+  this.isbn = book.isbn;
+  this.image_url = book.image_url;
+  this.description = book.description || 'description error';
+  console.log('===', this.description);
+  this.bookshelf = bookshelf ? bookshelf : 'unassigned';
+  this.id = book.id ? book.id : book.isbn;
+}
 
 app.listen( PORT, () => console.log(`APP is up on PORT:${PORT}`));
