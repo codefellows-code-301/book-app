ba097aa06d7d1b4fdc2f7ef47d9c26060c55b132
Erin Trainor <trainor.erin@gmail.com>
2019-01-03 21:26:20 -0800

    started form

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-$('.details-form-button').on('click', function(){
+$('.form-button').on('click', function(){
   console.log('++++button clicked+++++')
   $(this).next().removeClass('details-form');
 });
